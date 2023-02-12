ffcfc0d45bb4459a370237ddbcb5b9fed2991817
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-12 20:00:16 -0300

    New functions added

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html'
+})
+export class PaisInputComponent {
+  termino: string = '';
+
+  buscar() {
+
+  }
+}
