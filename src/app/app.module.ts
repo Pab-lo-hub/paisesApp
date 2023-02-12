319a74edf168049eddefbb3f15d42e7648639354
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-12 13:47:56 -0300

    new function to search by country

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     PaisModule,
+    HttpClientModule,
     SharedModule,
     AppRoutingModule,
   ],
