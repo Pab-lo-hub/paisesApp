e1c472576344a64da3d85c218d9ea46c5da11290
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-13 10:44:04 -0300

    Fixing details

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -13,13 +13,14 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService) { }
 
-  buscar() {
+  buscar( termino: string) {
     this.hayError = false;
-    console.log(this.termino);
-    this.paisService.buscarPais(this.termino)
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
       .subscribe((paises) => {
-        this.paises = paises;
         console.log(paises);
+        this.paises = paises;
       }, (err) => {
         this.hayError = true;
         this.paises = [];
